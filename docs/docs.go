db5a03b857ad2423456609ecc02e5f51dca6e80e
almasdjan <almasiitu@gmail.com>
2024-05-27 22:43:59 +0500

    host swagger

--- docs/docs.go
+++ docs/docs.go
@@ -2533,7 +2533,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "ozinshetestapi.modydev.kz",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Ozinshe",
